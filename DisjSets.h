a3a0b4cc88f9bbc0064ec35ba01d7f4a0e0cc8eb
sandersonraul <raul.oliveira26@gmail.com>
2022-03-24 13:48:57 -0300

    adding maze

--- DisjSets.h
+++ DisjSets.h
@@ -1,26 +1,105 @@
 /**
- * Disjoint set class
- * Ue union by rank and path compression.
+ * Disjoint set class.
+ * Use union by rank and path compression.
  * Elements in the set are numbered starting at 0.
- * 
- * 
  */
 
-#include<vector>
-class DisjSets{
-    public:
-       // Generates the disjoint sets, one for each element.
-        explicit DisjSets(int tamanho);
-        //return set's name containing x
-        int find(int x);
-        //return set's name containing x, with path compression.
-        int find(int x);
-        // Return the current number of disjoint sets
-        int size()const;
+#include <vector>
+#include <iostream>
+using namespace std;
 
-        int unir_altura(int raiz1,int raiz2);
+class DisjSets
+{
+public:
+  // Generates the disjoint sets, one for each element.
+  explicit DisjSets(int tamanho)
+  {
+    for (int i = 0; i < tamanho; i++)
+    {
+      vetor.push_back(i);
+    }
+  }
 
-        private:
-           int* vetor
+  // acha apenas o pai
+  // int find(int x) const
+  // {
+  //   if (vetor[x] < 0)
+  //   {
+  //     return x;
+  //   }
+  //   else
+  //   {
+  //     return find(vetor[x]);
+  //   }
+  // }
 
-} ;
+  int find(int x)
+  {
+    int aux;
+    aux = x;
+    while (vetor.at(aux) != aux)
+    {
+      aux = vetor.at(aux); // aux vai armazenar o pai do conjunto
+    }
+    int aux2;
+    while (vetor.at(x) != x)
+    {
+      aux2 = x;
+      x = vetor.at(x);
+      vetor.at(aux2) = aux; // todos vao apontar pro pai do conjunto
+    }
+    return aux;
+  }
+  // Return set's name containing x, with path compression.
+
+  // Merge two sets, by size or by estimated heigh (also knwon as rank).
+  void unir_altura(int raiz1, int raiz2)
+  {
+    if (vetor[raiz2] < vetor[raiz1])
+    {
+      vetor[raiz1] = raiz2;
+    }
+    else
+    {
+      if (vetor[raiz1] == vetor[raiz2])
+      {
+        vetor[raiz1] = vetor[raiz1] - 1;
+      }
+      vetor[raiz2] = raiz1;
+    }
+  }
+
+  // Return the current number of disjoint sets
+  int size()
+  {
+    vector<int> vector_conjuntos;
+
+    int pai;
+    int tamanho = 0;
+    bool verifica;
+    for (int i = 0; i < vetor.size(); i++)
+    {
+      pai = this->find(i);
+      verifica = false;
+      for (int j = 0; j < vector_conjuntos.size(); j++)
+      {
+        if (pai == vector_conjuntos.at(j))
+        {
+          verifica = true;
+          break;
+        }
+      }
+
+      if (verifica == false)
+      {
+        vector_conjuntos.push_back(pai);
+        tamanho++;
+      }
+    }
+
+    return tamanho;
+  }
+
+private:
+  vector<int> vetor;
+};
--- maze.cpp
+++ maze.cpp
@@ -1,9 +1,6 @@
-#include<iostream>
-#include"DisjSets.h"
-#include <stack>
-#include <utility>    
-#include <random>     
-#include <string>     
+#include <iostream> 
+#include <utility>  
+#include <string>   
 #include <cassert>
 #include <vector>
 #include <algorithm>
@@ -12,29 +9,483 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <unistd.h>
-int numeroAleatorio(int menor, int maior) {
-       return rand()%(maior-menor+1) + menor;
+#include "DisjSets.h"
+#include <stack> 
+
+
+int numeroAleatorio(int menor, int maior)
+{
+    return rand() % (maior - menor + 1) + menor;
 }
-int main (int argc, char** argv){
+// Utilizando código da internet
+void drawMaze(int altura, int largura, unsigned char Maze[], unsigned char TopWall, unsigned char RightWall, unsigned char BottomWall, unsigned char LeftWall)
+{
+    system("clear");
+    for (int i = 0; i < altura; i++)
+    {
+        for (int j = largura * i; j < largura * i + largura; j++)
+        {
+            if (Maze[j] & TopWall)
+            {
+                std::cout << "####";
+            }
+            else
+            {
+                std::cout << "#  #";
+            }
+        }
+        std::cout << std::endl;
+        for (int j = largura * i; j < largura * i + largura; j++)
+        {
+            if (Maze[j] & LeftWall)
+            {
+                std::cout << "# ";
+            }
+            else
+            {
+                std::cout << "  ";
+            }
+            if (Maze[j] & RightWall)
+            {
+                std::cout << " #";
+            }
+            else
+            {
+                std::cout << "  ";
+            }
+        }
+        std::cout << std::endl;
+        for (int j = largura * i; j < largura * i + largura; j++)
+        {
+            if (Maze[j] & BottomWall)
+            {
+                std::cout << "####";
+            }
+            else
+            {
+                std::cout << "#  #";
+            }
+        }
+        std::cout << std::endl;
+    }
+    std::cout << std::endl;
+}
+
+int main(int argc, char **argv)
+{
     unsigned char TopWall = 1;
     unsigned char RightWall = 2;
     unsigned char BottomWall = 4;
     unsigned char LeftWall = 8;
-    unsigned char trackedPath = 16;
-    unsigned char wrongTrackedPath = 32;
-    std::vector<int> v; //arMazena os números embaralhados aleatoreamente
-    int altura=10;
-    int largura=10;
-    bool valido=false; //indica se o labirinto será valido ou totalmente conexo
+    unsigned char celulasVisitadas = 16;
+    unsigned char celulasErradas = 32; // arMazena os números embaralhados aleatoreamente
+    int altura = 5;
+    int largura = 5;
+    bool valido = false; // indica se o labirinto será valido ou totalmente conexo
 
-     int tamanho=altura * largura;
-    DisjSets conjuntos(tamanho);
-    unsigned char Maze[ tamanho];
-        for ( int i=0; i < (tamanho); i++ ){
-            Maze[ i ] = TopWall | RightWall | BottomWall | LeftWall;
-            v.push_back(i); //preenche vetor v com os numeros de todas as celulas
+
+
+    // Utilizando código da internet
+    if (argc > 4)
+    {
+        std::cout << "Há argumentos demais" << std::endl;
+        return 0;
+    }
+    else if (argc == 4)
+    {
+        if (isdigit(argv[1][0]))
+        {
+            altura = atoi(argv[1]);
+            if (isdigit(argv[2][0]))
+            {
+                largura = atoi(argv[2]);
+                valido = true;
+            }
+            else
+            {
+                valido = true;
+                largura = atoi(argv[3]);
+            }
         }
-        Maze[0] &= ~LeftWall; //apaga primeira parede
-        Maze[tamanho-1] &= ~RightWall; //apaga ultima parede
-}
+        else
+        {
+            valido = true;
+            altura = atoi(argv[2]);
+            largura = atoi(argv[3]);
+        }
+    }
+    else if (argc == 3)
+    {
+        if (isdigit(argv[1][0]))
+        {
+            altura = atoi(argv[1]);
+            if (isdigit(argv[2][0]))
+            {
+                largura = atoi(argv[2]);
+            }
+            else
+            {
+                valido = true;
+            }
+        }
+        else
+        {
+            valido = true;
+            altura = atoi(argv[2]);
+        }
+    }
+    else if (argc == 2)
+    {
+        if (isdigit(argv[1][0]))
+        {
+            altura = atoi(argv[1]);
+        }
+        else
+        {
+            valido = true;
+        }
+    }
 
+    if (altura > 100 || largura > 100)
+    {
+        std::cout << "O limite das dimensões é 100 para a lagura e 100 para a altura" << std::endl;
+        return 0;
+    }
+
+    int tamanho = altura * largura;
+    DisjSets conjuntos(tamanho);
+    unsigned char Maze[tamanho];
+    for (int i = 0; i < (tamanho); i++)
+    {
+        Maze[i] = TopWall | RightWall | BottomWall | LeftWall;
+        // preenche vetor v com os numeros de todas as celulas
+    }
+    Maze[0] &= ~LeftWall;            // apaga primeira parede
+    Maze[tamanho - 1] &= ~RightWall; // apaga ultima parede
+
+    // embaralha numeros do vetor v
+
+    int randomCell = numeroAleatorio(0, tamanho - 1);
+    unsigned char parede;
+    //////////***********valido**************//////////////
+    if (valido)
+    {
+        while (conjuntos.find(0) != conjuntos.find(tamanho - 1))
+        {                                   // enquanto o conjunto da primeira casa for diferente do da última casa
+            parede = numeroAleatorio(0, 3); // sorteia um numero entre 0 e 3, pra decidir qual parede quebrar
+            switch (parede)
+            {
+            case 0: // TopWall
+                if (randomCell > largura - 1)
+                { // se tiver na primeira linha, nao quebra a parede de cima
+                    if (Maze[randomCell] & TopWall)
+                    { // A parede está intacta?
+                        if (conjuntos.find(randomCell) != conjuntos.find(randomCell - largura))
+                        {
+                            Maze[randomCell] &= ~TopWall; // quebra a parede
+                            Maze[randomCell - largura] &= ~BottomWall;
+                            conjuntos.unir_altura(conjuntos.find(randomCell), conjuntos.find(randomCell - largura));
+                        }
+                    }
+                }
+                break;
+            case 1: // RightWall
+                if ((randomCell + 1) % largura != 0)
+                { // se tiver na ultima coluna, nao quebra a parede de cima
+                    if (Maze[randomCell] & RightWall)
+                    { // A parede está intacta?
+                        if (conjuntos.find(randomCell) != conjuntos.find(randomCell + 1))
+                        {
+                            Maze[randomCell] &= ~RightWall; // quebra a parede
+                            Maze[randomCell + 1] &= ~LeftWall;
+                            conjuntos.unir_altura(conjuntos.find(randomCell), conjuntos.find(randomCell + 1));
+                        }
+                    }
+                }
+                break;
+            case 2:
+                if (randomCell < tamanho - largura - 1)
+                { // se tiver na ultima linha, nao quebra a parede de cima
+                    if (Maze[randomCell] & BottomWall)
+                    { // A parede está intacta?
+                        if (conjuntos.find(randomCell) != conjuntos.find(randomCell + largura))
+                        {
+                            Maze[randomCell] &= ~BottomWall; // quebra a parede
+                            Maze[randomCell + largura] &= ~TopWall;
+                            conjuntos.unir_altura(conjuntos.find(randomCell), conjuntos.find(randomCell + largura));
+                        }
+                    }
+                }
+                break;
+            case 3:
+                if (randomCell % largura != 0)
+                { // se tiver na primeira coluna, nao quebra a parede de cima
+                    if (Maze[randomCell] & LeftWall)
+                    { // A parede está intacta?
+                        if (conjuntos.find(randomCell) != conjuntos.find(randomCell - 1))
+                        {
+                            Maze[randomCell] &= ~LeftWall; // quebra a parede
+                            Maze[randomCell - 1] &= ~RightWall;
+                            conjuntos.unir_altura(conjuntos.find(randomCell), conjuntos.find(randomCell - 1));
+                        }
+                    }
+                }
+                break;
+            default:
+                break;
+            }
+
+            // Desenha
+            drawMaze(altura, largura, Maze, TopWall, RightWall, BottomWall, LeftWall);
+            std::cout << "Qtd de conjuntos: " << conjuntos.size() << std::endl;
+            usleep(100000);
+        }
+    }
+    else
+    {
+        //////////*********totalmente conexo******/////////////
+        randomCell = 0;
+        // unsigned char parede;
+        while (conjuntos.size() != 1)
+        {                                   // enquanto não houver apenas um conjunto
+            parede = numeroAleatorio(0, 3); // sorteia um numero entre 0 e 3, pra decidir qual parede quebrar
+            switch (parede)
+            {
+            case 0: // TopWall
+                if (randomCell > largura - 1)
+                { // se tiver na primeira linha, nao quebra a parede de cima
+                    if (Maze[randomCell] & TopWall)
+                    { // A parede está intacta?
+                        if (conjuntos.find(randomCell) != conjuntos.find(randomCell - largura))
+                        {
+                            Maze[randomCell] &= ~TopWall; // quebra a parede
+                            Maze[randomCell - largura] &= ~BottomWall;
+                            conjuntos.unir_altura(conjuntos.find(randomCell), conjuntos.find(randomCell - largura));
+                        }
+                    }
+                }
+                break;
+            case 1: // RightWall
+                if ((randomCell + 1) % largura != 0)
+                { // se tiver na ultima coluna, nao quebra a parede de cima
+                    if (Maze[randomCell] & RightWall)
+                    { // A parede está intacta?
+                        if (conjuntos.find(randomCell) != conjuntos.find(randomCell + 1))
+                        {
+                            Maze[randomCell] &= ~RightWall; // quebra a parede
+                            Maze[randomCell + 1] &= ~LeftWall;
+                            conjuntos.unir_altura(conjuntos.find(randomCell), conjuntos.find(randomCell + 1));
+                        }
+                    }
+                }
+                break;
+            case 2:
+                if ((randomCell) < tamanho - largura - 1)
+                { // se tiver na ultima linha, nao quebra a parede de cima
+                    if (Maze[randomCell] & BottomWall)
+                    { // A parede está intacta?
+                        if (conjuntos.find(randomCell) != conjuntos.find(randomCell + largura))
+                        {
+                            Maze[randomCell] &= ~BottomWall; // quebra a parede
+                            Maze[randomCell + largura] &= ~TopWall;
+                            conjuntos.unir_altura(conjuntos.find(randomCell), conjuntos.find(randomCell + largura));
+                        }
+                    }
+                }
+                break;
+            case 3:
+                if ((randomCell) % largura != 0)
+                { // se tiver na primeira coluna, nao quebra a parede de cima
+                    if (Maze[randomCell] & LeftWall)
+                    { // A parede está intacta?
+                        if (conjuntos.find(randomCell) != conjuntos.find(randomCell - 1))
+                        {
+                            Maze[randomCell] &= ~LeftWall; // quebra a parede
+                            Maze[randomCell - 1] &= ~RightWall;
+                            conjuntos.unir_altura(conjuntos.find(randomCell), conjuntos.find(randomCell - 1));
+                        }
+                    }
+                }
+                break;
+            default:
+                break;
+            }
+
+            // Desenha
+            drawMaze(altura, largura, Maze, TopWall, RightWall, BottomWall, LeftWall);
+            std::cout << "Qtd de conjuntos: " << conjuntos.size() << std::endl;
+            usleep(100000);
+            randomCell++;
+            if (randomCell == (tamanho - 1))
+            {
+                randomCell = 0;
+            }
+        }
+    }
+    // Utilizando código da internet
+    std::cout << "Digite f para enrandomCellrar a saída " << std::endl;
+    char comando;
+    std::cin >> comando;
+
+    if (comando == 'f')
+    {
+
+        // backtracking
+        std::stack<int> path; // pilha que armazena o caminho sendo percorrido
+
+        int c = 0;    // armazena a posicao que esta sendo percorrida na matriz Maze
+        path.push(c); // adiciona a posicao 0 na pilha
+        Maze[c] |= celulasVisitadas;
+        while (path.top() != tamanho - 1)
+        {
+
+            if (!(Maze[c] & RightWall) && !(Maze[c + 1] & celulasVisitadas))
+            {        // se não tiver parede a direita e a direita ainda nao tiver sido percorrida
+                c++; // posicao anda uma coluna
+                path.push(c);
+                Maze[c] |= celulasVisitadas; // marca essa posicao como percorrida
+            }
+            else if (!(Maze[c] & BottomWall) && !(Maze[c + largura] & celulasVisitadas))
+            {                    // se nao tiver a parede de baixo e a celular de baixo ainda nao tiver sido percorrida
+                c = c + largura; // posicao desce uma linha
+                path.push(c);
+                Maze[c] |= celulasVisitadas;
+            }
+            else if (!(Maze[c] & LeftWall) && !(Maze[c - 1] & celulasVisitadas))
+            {              // se nao tiver a parede a esquerda e a esquerda nao tiver sido percorrida
+                c = c - 1; // posicao volta uma coluna
+                path.push(c);
+                Maze[c] |= celulasVisitadas;
+            }
+            else if (!(Maze[c] & TopWall) && !(Maze[c - largura] & celulasVisitadas))
+            { // se nao tiver a parede de cima e a celula de cima nao tiver sido percorrida
+                c = c - largura;
+                path.push(c);
+                Maze[c] |= celulasVisitadas;
+            }
+            else if (!(Maze[c] & RightWall) && (Maze[c + 1] & celulasVisitadas) && !(Maze[c + 1] & celulasErradas))
+            {
+                Maze[c] |= celulasErradas;
+                path.pop();
+                c = c + 1;
+            }
+            else if (!(Maze[c] & BottomWall) && (Maze[c + largura] & celulasVisitadas) && !(Maze[c + largura] & celulasErradas))
+            {
+                Maze[c] |= celulasErradas;
+                path.pop();
+                c = c + largura;
+            }
+            else if (!(Maze[c] & LeftWall) && (Maze[c - 1] & celulasVisitadas) && !(Maze[c - 1] & celulasErradas))
+            {
+                Maze[c] |= celulasErradas;
+                path.pop();
+                c = c - 1;
+            }
+            else if (!(Maze[c] & TopWall) && (Maze[c - largura] & celulasVisitadas) && !(Maze[c - largura] & celulasErradas))
+            {
+                Maze[c] |= celulasErradas;
+                path.pop();
+                c = c - largura;
+            }
+
+
+            system("clear");
+            // Desenha segunda opcao
+            for (int i = 0; i < altura; i++)
+            {
+                for (int j = largura * i; j < largura * i + largura; j++)
+                {
+                    if (Maze[j] & TopWall)
+                    {                        // se tem a parede de cima
+                        std::cout << "####"; // desenha a parede de cima
+                    }
+                    else
+                    {
+                        std::cout << "#  #";
+                    } // senao desenha apenas as diagonais
+                }
+                std::cout << std::endl;
+                for (int j = largura * i; j < largura * i + largura; j++)
+                {
+                    if (Maze[j] & LeftWall)
+                    {
+                        if (Maze[j] & celulasErradas)
+                        {
+                            std::cout << "#*";
+                        }
+                        else if (Maze[j] & celulasVisitadas)
+                        {
+                            std::cout << "#@";
+                        }
+                        else
+                        {
+                            std::cout << "# ";
+                        }
+                    }
+                    else
+                    {
+                        if (Maze[j] & celulasErradas)
+                        {
+                            std::cout << " *";
+                        }
+                        else if (Maze[j] & celulasVisitadas)
+                        {
+                            std::cout << " @";
+                        }
+                        else
+                        {
+                            std::cout << "  ";
+                        }
+                    }
+                    if (Maze[j] & RightWall)
+                    {
+                        if (Maze[j] & celulasErradas)
+                        {
+                            std::cout << "*#";
+                        }
+                        else if (Maze[j] & celulasVisitadas)
+                        {
+                            std::cout << "@#";
+                        }
+                        else
+                        {
+                            std::cout << " #";
+                        }
+                    }
+                    else
+                    {
+                        if (Maze[j] & celulasErradas)
+                        {
+                            std::cout << "* ";
+                        }
+                        else if (Maze[j] & celulasVisitadas)
+                        {
+                            std::cout << "@ ";
+                        }
+                        else
+                        {
+                            std::cout << "  ";
+                        }
+                    }
+                }
+                std::cout << std::endl;
+                for (int j = largura * i; j < largura * i + largura; j++)
+                {
+                    if (Maze[j] & BottomWall)
+                    {
+                        std::cout << "####";
+                    }
+                    else
+                    {
+                        std::cout << "#  #";
+                    }
+                }
+                std::cout << std::endl;
+            }
+            std::cout << std::endl;
+            usleep(500000);
+        }
+    }
+
+    return 0;
+}
